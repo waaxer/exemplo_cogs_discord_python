f1b754e4ad4de778a59d4bebb8d7d3edf2c77bf6
Gustavo <86325229+waaxer@users.noreply.github.com>
2023-04-16 22:19:42 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+import asyncio
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.all()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='.', intents=intents, application_id=os.getenv('BOT_ID'))
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} Está online e pronto para uso.')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Sincronizado {len(synced)} comando(s)!")
+    except Exception as erro:
+        print(f"Erro ao sincronizar comandos: {erro}")     
+
+async def load():
+    for filename in os.listdir('./commands'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'commands.{filename[:-3]}')
+
+async def main():
+    await load()
+    await bot.start(os.getenv('BOT_SECRET'))
+
+asyncio.run(main())
