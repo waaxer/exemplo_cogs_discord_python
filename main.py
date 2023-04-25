32f64d2a2170dd20fb6048bae910457af243995d
Gustavo <86325229+waaxer@users.noreply.github.com>
2023-04-25 10:23:48 -0300

    Funcionalidade para pastas de eventos

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ async def load():
     for filename in os.listdir('./commands'):
         if filename.endswith('.py'):
             await bot.load_extension(f'commands.{filename[:-3]}')
+    for filename in os.listdir('./events'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'events.{filename[:-3]}')
 
 async def main():
     await load()
